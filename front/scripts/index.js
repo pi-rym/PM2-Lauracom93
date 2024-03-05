d2f6a3ab6bf97bd80d5fb2eb62cd4fd39669edd3
lauracom93 <laura.combitac@gmail.com>
2024-03-05 00:07:15 -0500

    avance1 proyecto M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,16 @@
 console.log(tempData);
+const elementos = document.getElementById("tarjetas");
+tempData.forEach(movie=>{
+const cartas = document.createElement("div");
+infomovie.classlit.add("infomovie");
+const poster = document.createElement("img");
+poster.src = movie.poster;
+poster.alt = movie.title;
+const title = document.createElement("h2");
+movietitle.texcontent = movietitle;
+
+
+cartas.appendChild(poster);
+cartas.appendChild(title);
+elementos.appendChild(cartas);
+});
